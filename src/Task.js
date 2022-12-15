102237bc60c55e1264faae1a38b0a5fd2c14ea1a
amalia <amalia.19012@mhs.unesa.ac.id>
2022-12-15 14:48:52 +0700

    commit ketiga

--- src/Task.js
+++ src/Task.js
@@ -1,4 +1,4 @@
-const url = "-" // TODO: replace this
+const url = "https://relaxed-souffle-f240b1.netlify.app/" // TODO: replace this
 const studentName = "Amalia Fandaning Tyas" // TODO: replace this
 const studentId = "FE2663892" // TODO: replace this
 
